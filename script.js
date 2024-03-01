acce9fe056778405ad42187eb5b62f73ad7e4b37
Kashfi Fahim <Kashfi.Fahim@gmail.com>
2024-03-01 16:27:19 -0500

    up to step 11

---
+++ script.js
@@ -0,0 +1,21 @@
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+    event.preventDefault();
+    const inputValues = [...document.getElementsByClassName("values-dropdown")]
+                        .map(
+                            (dropdown) => Number(dropdown.value)
+                            );
+
+};
+
+const updateUI = (array = []) => {
+    array.forEach(
+        (num, i) => {
+            const outputValueNode = document.getElementById(`output-value-${i}`);
+        }
+    );
+};
+
+sortButton.addEventListener("click", sortInputArray);
+
